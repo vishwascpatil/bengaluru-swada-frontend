7591044f24a493e290ef4aa09f11e3ceb5172bf7
vishwascpatil <vishwascpatil@gmail.com>
2020-07-14 23:55:56 +0530

    shruthi changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'bengaluru-swada-frontend';
+  title = 'shruthi r valsange built this website';
 }
