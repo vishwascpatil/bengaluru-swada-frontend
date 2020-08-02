600d60b7b18a54dc3c670763e660ffdde7090607
vishwascpatil <vishwascpatil@gmail.com>
2020-08-02 18:28:49 +0530

    area-details

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SearchLocationService } from '../search-location/search-location.service';
+import { HomePageService } from './home-page.service';
 
 @Component({
   selector: 'app-home-page',
@@ -10,11 +11,19 @@ import { SearchLocationService } from '../search-location/search-location.servic
 export class HomePageComponent implements OnInit {
   constructor(
     private router: Router,
-    private searchLocation: SearchLocationService
+    private searchLocation: SearchLocationService,
+    private homepageService: HomePageService
   ) {}
 
+  areaDetails: any = null;
   ngOnInit(): void {
-    this.searchLocation.locationname;
+    if (this.searchLocation.locationname.length > 0) {
+      this.homepageService
+        .GetLocationDetails(this.searchLocation.locationname)
+        .subscribe((areaDetails) => {
+          this.areaDetails = areaDetails;
+        });
+    }
   }
 
   navigate(): void {
--- src/app/home-page/home-page.service.ts
+++ src/app/home-page/home-page.service.ts
@@ -1,37 +1,18 @@
-import { Injectable, ErrorHandler } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { tap, catchError, map } from 'rxjs/operators';
 
-export interface MapBoxOutPut {
-  attribution: string;
-  features: Feature[];
-  query: [];
-}
-
-export interface Feature {
-  place_name: string;
-}
 @Injectable({
   providedIn: 'root',
 })
-export class SearchLocationService {
+export class HomePageService {
   constructor(private http: HttpClient) {}
 
-  // search_word(query: string) {
-  //   const url =
-  //     'https://bengaluruswada.herokuapp.com/api/BengaluruStreetFood/locations';
-  //   return this.http.get(url).pipe(
-  //     map((res: MapBoxOutPut) => {
-  //       return res.features;
-  //     })
-  //   );
-  // }
-  locationname: string = '';
   private productsUrl =
-    'https://bengaluruswada.herokuapp.com/api/BengaluruStreetFood/locations';
-  GetLocations(): Observable<any> {
-    return this.http.get<any>(this.productsUrl).pipe(
+    'https://bengaluruswada.herokuapp.com/api/BengaluruStreetFood/getareadetails/';
+  GetLocationDetails(locationName: string): Observable<any> {
+    return this.http.get<any>(this.productsUrl + locationName).pipe(
       tap((data) => console.log('All : ' + JSON.stringify(data))),
       catchError(this.handleError)
     );
